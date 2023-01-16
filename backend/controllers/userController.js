b8d3c8e589ea79f8a93919e059ba132f10dac7e7
DaddaAdam <daddaadam@gmail.com>
2023-01-16 17:00:20 +0100

    added update user profile endpoint in the backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,4 +76,30 @@ const getUserProfile = expressAsyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser };
+// @desc    Modifier le profil d'un utilisateur
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = expressAsyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) user.password = req.body.password;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("ERREUR: données invalides.");
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile };
